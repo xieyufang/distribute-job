a6dc0614b9a3e14da80adf0d2138d1fffc9051e8
xieyufang <xie.yufang@163.com>
2017-05-03 17:46:24 +0800

    configuration files

---
+++ distribute-job-web/src/main/java/cn/xyf/job/web/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package cn.xyf.job.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by xieyufang on 17/5/3.
+ */
+@Controller
+public class AuthenticationController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/login")
+    public String handler(){
+        System.out.println("test");
+        return "logout";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="")
+    public String login(){
+
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="")
+    public String logout(){
+        return null;
+    }
+}
