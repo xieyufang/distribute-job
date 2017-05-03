b644c02f62b5da8d1b27e85fba2df5d3c9ecb89c
xieyufang <xie.yufang@163.com>
2017-05-03 11:56:03 +0800

    second

---
+++ distribute-job-web/src/test/java/cn/xyf/AppTest.java
@@ -0,0 +1,17 @@
+package cn.xyf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by xieyufang on 17/5/3.
+ */
+public class AppTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AppTest.class);
+
+
+    public static void main(String[] args) {
+        LOG.info("test");
+    }
+}
